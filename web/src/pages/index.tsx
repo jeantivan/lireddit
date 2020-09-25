b3b6618c08b8e3c7f49321d14f6eb63d92c5337b
jptivan53 <jptivan53@gmail.com>
2020-09-24 20:27:43 -0400

    Muestra el mensaje de error

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -14,12 +14,17 @@ const Index = () => {
     cursor: null as string | null,
   });
 
-  const [{ data, fetching }] = usePostsQuery({
+  const [{ data, error, fetching }] = usePostsQuery({
     variables,
   });
 
   if (!fetching && !data) {
-    return <div>Query failed for some reason</div>;
+    return (
+      <>
+        <div>Query failed for some reason</div>
+        <div>{error?.message}</div>
+      </>
+    );
   }
 
   return (
