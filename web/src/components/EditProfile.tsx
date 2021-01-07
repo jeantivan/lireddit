e9f2e3684fd65f0a850ab523b9c67ec1ce98c5a6
jptivan53 <jptivan53@gmail.com>
2021-01-06 20:17:26 -0400

    Modal para editar los datos de un usuario

--- web/src/components/EditProfile.tsx
+++ web/src/components/EditProfile.tsx
@@ -1,7 +1,9 @@
-import { UserProfileQuery } from "@/generated/graphql";
 import {
   Box,
   Button,
+  FormControl,
+  FormLabel,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -9,10 +11,18 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Image,
+  IconButton,
+  Flex,
+  Grid,
+  GridItem,
 } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import React from "react";
-import { InputField } from "./InputField";
+import { BsCamera } from "react-icons/bs";
+import { InputField, TextareaField } from "./InputField";
+
+const URL_PREFIX = "http://localhost:4000";
 
 interface EditProfileProps {
   profile: any;
@@ -25,19 +35,22 @@ export const EditProfile: React.FC<EditProfileProps> = ({
   isOpen,
   onClose,
 }) => {
-  const focusRef = React.useRef(null);
+  const [file, setFile] = React.useState("");
+  React.useEffect(() => {
+    console.log({ profile });
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target && e.target.files) {
+      console.log({ file: e.target.files[0] });
+      setFile(URL.createObjectURL(e.target.files[0]));
+    }
+    return;
+  };
   return (
-    <Modal
-      size="xl"
-      scrollBehavior="outside"
-      blockScrollOnMount
-      isOpen={isOpen}
-      onClose={onClose}
-      initialFocusRef={focusRef}
-    >
+    <Modal size="xl" isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
+        <ModalHeader>Editar Perfil</ModalHeader>
         <ModalCloseButton />
         <Formik
           initialValues={{ ...profile }}
@@ -48,20 +61,72 @@ export const EditProfile: React.FC<EditProfileProps> = ({
           {({ isSubmitting }) => (
             <Form>
               <ModalBody>
-                <InputField
-                  name="fullName"
-                  placeholder="Full name"
-                  label="Full Name"
-                />
-                <Box my={4}>
-                  <div ref={focusRef} />
-                  <InputField
-                    textarea
-                    name="description"
-                    placeholder="Tell me about you"
-                    label="Description"
-                  />
-                </Box>
+                <Grid
+                  templateRows="repeat(4, auto)"
+                  templateColumns="repeat(3, 1fr)"
+                  gap={2}
+                >
+                  <GridItem rowSpan={2} colSpan={1}>
+                    <FormLabel>Profile Picture</FormLabel>
+                    <Box
+                      p={1}
+                      w="140px"
+                      h="140px"
+                      bg="gray.400"
+                      borderRadius="100%"
+                      position="relative"
+                    >
+                      <Image
+                        borderRadius="100%"
+                        boxSize="100%"
+                        objectFit="cover"
+                        src={file !== "" ? file : URL_PREFIX + profile.imageUrl}
+                        alt={profile.user.username}
+                      />
+                      <label htmlFor="profile-picture">
+                        <IconButton
+                          colorScheme="teal"
+                          isRound
+                          position="absolute"
+                          size="lg"
+                          top="100%"
+                          left="100%"
+                          transform="translate(-80%, -100%)"
+                          aria-label="Upload profile picture"
+                          icon={<BsCamera />}
+                          cursor="pointer"
+                          as="span"
+                        />
+                        <Box display="none">
+                          <Input
+                            accept="image/*"
+                            id="profile-picture"
+                            name="profile-picture"
+                            type="file"
+                            onChange={handleChange}
+                          />
+                        </Box>
+                      </label>
+                    </Box>
+                  </GridItem>
+                  <GridItem colSpan={2}>
+                    <InputField
+                      name="fullName"
+                      placeholder="Full name"
+                      label="Full Name"
+                    />
+                  </GridItem>
+                  <GridItem colSpan={2}>
+                    <InputField name="birthday" type="date" label="Birthday" />
+                  </GridItem>
+                  <GridItem rowSpan={2} colSpan={3}>
+                    <TextareaField
+                      name="description"
+                      placeholder="Tell people something about you"
+                      label="Description"
+                    />
+                  </GridItem>
+                </Grid>
               </ModalBody>
 
               <ModalFooter>
